70d00fa44cdfad397565332d06fb654ba58431e6
Tomas Susanka <tsusanka@gmail.com>
2017-01-11 17:13:47 +0100

    Migrations: users, notes

--- migrations/structures/2016-08-22-151053-init.sql
+++ migrations/structures/2016-08-22-151053-init.sql
@@ -1,1 +1,19 @@
--- todo
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(255) NOT NULL,
+  `registerd_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649D1B862B8` (`hash`),
+);
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `url` longtext NOT NULL,
+  `created_at` datetime NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_CFBDFA14A76ED395` (`user_id`),
+  CONSTRAINT `FK_CFBDFA14A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
